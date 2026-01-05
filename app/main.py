abfd3703f296b466da38ad026e29dc61b12771bb
vikas-pragadas <vikas.s@pragadastech.com>
2026-01-05 15:51:19 +0530

    second ci/cd test

--- app/main.py
+++ app/main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+def home():
+    return {"message": "Welcome to the API"}
+
 @app.get("/hello")
 def read_hello():
     return {"message": "hello word"}
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,6 +4,11 @@ from app.main import app
 
 client = TestClient(app)
 
+def test_home_route():
+    resp = client.get("/")
+    assert resp.status_code == 200
+    assert resp.json() == {"message": "Welcome to the API"}
+
 def test_hello_route():
     resp = client.get("/hello")
     assert resp.status_code == 200
